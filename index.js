94cb60272c2009c545f63f1e51d9ad2430979d89
Eli Craig <elidcraig@gmail.com>
2022-05-06 10:40:58 -0700

    commit tree 9413558dcec638fde32383f1162158c4e2d0b9d6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(usernameStr, emailStr){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: usernameStr,
+            email: emailStr
+        })
+    })
+        .then(resp => resp.json())
+        .then(newUserObject => appendItem(newUserObject.id))
+        .catch(errorObj => appendItem(errorObj.message))
+}
+
+function appendItem(item){
+    const itemContainer = document.createElement('p')
+    itemContainer.textContent = item
+    document.body.appendChild(itemContainer)
+}
